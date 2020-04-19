544ad13186ed57699c10fd0e7e34569d4c83c7a4
radebit <26564356@qq.com>
2020-04-19 23:35:07 +0800

    完成SessionStorage封装

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,21 @@
   </div>
 </template>
 
+<script>
+  // import storage from './storage/index'
+  export default {
+    name:'app',
+    components:{
+    },
+    mounted() {
+      // storage.setItem('a','1')
+      // storage.setItem('user',{a:1})
+      // storage.setItem('abc',{a:1},'user')
+      // storage.clear('abc','user')
+    }
+  }
+</script>
+
 <style lang="scss">
 
 </style>
---
+++ src/storage/index.js
@@ -0,0 +1,40 @@
+/**
+ * Storage封装
+ **/
+const STORAGE_KEY = 'shopMi';
+export default {
+    //存储值
+    setItem(key, value, moduleName) {
+        if (moduleName) {
+            let val = this.getItem(moduleName);
+            val[key] = value;
+            this.setItem(moduleName, val);
+        } else {
+            let val = this.getStorage();
+            val[key] = value;
+            window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val));
+        }
+    },
+    //获取值
+    getItem(key, moduleName) {
+        if (moduleName) {
+            let val = this.getItem(moduleName)
+            if (val) return val[key];
+        }
+        return this.getStorage()[key];
+    },
+    //获取storage
+    getStorage() {
+        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}');
+    },
+    //清空值
+    clear(key, moduleName) {
+        let val = this.getStorage();
+        if (moduleName) {
+            delete val[moduleName][key];
+        } else {
+            delete val[key];
+        }
+        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val));
+    }
+}
